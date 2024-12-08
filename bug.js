7747070807cefec36bd2c58aef471aabddfd23cf
BugHunter-X <askzenai@gmail.com>
2024-12-08 20:02:06 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,13 @@
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b; 
+}
+
+console.log(add(5, 3)); // Output: 8
+console.log(subtract(5, 3)); // Output: 2
+
+//This is a simple example but it can be very difficult to debug when dealing with complex programs
+//The issue is that the subtract function doesn't work correctly when subtracting negative numbers
